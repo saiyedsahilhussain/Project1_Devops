5b9bcd28bf42c4a76355aebb2191b74cf11a1a32
saiyedsahilhussain <96873337+saiyedsahilhussain@users.noreply.github.com>
2025-03-20 02:00:23 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // Set mode to either 'development' or 'production'
-  entry: 'index.js', // Your entry file, make sure 'src/index.js' exists
+  entry: './src/index.js', // Your entry file, make sure 'src/index.js' exists
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
